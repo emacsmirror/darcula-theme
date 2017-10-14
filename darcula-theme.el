6e56010ad23726b1b354e6fd31b8bbf2c7d218e9
Sam Halliday <sam.halliday@gmail.com>
2017-10-14 10:05:12 +0100

    brighter code colours in org-mode

--- darcula-theme.el
+++ darcula-theme.el
@@ -144,9 +144,10 @@
  '(company-tooltip-common ((t (:inherit 'mode-line-emphasis))))
  '(company-tooltip-common-selection ((t (:inherit 'highlight))))
  '(company-tooltip-annotation ((t (:inherit 'mode-line))))
- '(org-verbatim ((t (:background "#2F2F2F"))))
- '(markdown-code-face ((t (:inherit 'org-verbatim))))
- '(markdown-pre-face ((t (:inherit 'markdown-code-face))))
+ '(org-code ((t (:background "#2F2F2F"))))
+ '(org-verbatim ((t (:inherit 'org-code))))
+ '(markdown-code-face ((t (:inherit 'org-code))))
+ '(markdown-pre-face ((t (:inherit 'org-verbatim))))
  ;; http://www.gnu.org/software/emacs/manual/html_node/ediff/Highlighting-Difference-Regions.html
  '(ediff-current-diff-A ((t (:background "#3B2B2B"))))
  '(ediff-current-diff-B ((t (:background "#2B3B2B"))))
